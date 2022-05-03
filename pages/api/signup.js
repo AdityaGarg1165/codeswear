22b98d2b73ef5d7333f390465210fbe61ebb95d1
AdityaGarg1165 <90836787+AdityaGarg1165@users.noreply.github.com>
2022-05-03 16:55:33 +0530

    Update signup.js

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -3,8 +3,8 @@ const mongo = require('mongoose')
 const User = require('./User')
 const cryptojs = require("crypto-js")
 export default async function handler(req, res) {
-  let data = JSON.parse(req.body)
-  res.status(200).json(data)
+  // let data = JSON.parse(req.body)
+  res.status(200).json({data:"sdsdsdsdsdsdsdsdsdsd"})
     // const user = await User.create({
     //   "email":data["formemail"],
     //   "password":cryptojs.AES.encrypt(data["formpassword"],'khotakhota')
